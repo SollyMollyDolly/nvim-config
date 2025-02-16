a1ae34e74b21c875db3335a4f44bd95115eaf7bf
SollyMollyDolly <sollymollydolly@gmail.com>
2025-02-15 21:02:37 -0300

    ayaya!

--- init.lua
+++ init.lua
@@ -9,3 +9,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
     end,
 })
+
+vim.g.transparentbg = false
+
+function ToggleTransparency()
+    if vim.g.transparentbg == false then
+        vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+        vim.cmd("hi StatusLine guibg=NONE")  
+        vim.g.transparentbg = true
+    else
+        vim.cmd("hi Normal guibg=#212337") -- eldritch.nvim default background
+        vim.cmd("hi StatusLine guibg=NONE")  -- eldritch.nvim default statusline
+        vim.g.transparentbg = false
+    end
+end
+
+vim.cmd("command! ToggleTransparency lua ToggleTransparency()")
